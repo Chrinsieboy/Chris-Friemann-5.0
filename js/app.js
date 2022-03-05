335d0b26408dabc88f03ddbb92246fec30bb95b5
Chris Friemann <50674927+Chrinsieboy@users.noreply.github.com>
2022-03-05 14:34:10 +0100

    Feat: About me and navbar
    About me is now here. Navbar has been fixed for directions and style is for the about me

--- js/app.js
+++ js/app.js
@@ -1,7 +1,33 @@
+// Navbars
 function openNav() {
     document.getElementById("SideNavBar").style.width = "250px";
 }
 
 function closeNav() {
+    document.getElementById("SideNavBar").style.width = "0";
+}
+
+// Scrolling to the top of the page
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    document.getElementById("SideNavBar").style.width = "0";
+}
+
+// Scrolling to the about section
+function scrollToAbout() {
+    document.getElementById("About").scrollIntoView();
+    document.getElementById("SideNavBar").style.width = "0";
+}
+
+// Scrolling to the projects section
+function scrollToProjects() {
+    document.getElementById("Projects").scrollIntoView();
+    document.getElementById("SideNavBar").style.width = "0";
+}
+
+// Scrolling to the contact section
+function scrollToContact() {
+    document.getElementById("Contact").scrollIntoView();
     document.getElementById("SideNavBar").style.width = "0";
 }
